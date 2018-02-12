ca5a986b7eb2009c3388a96aa3dc210d2368d86b
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-12 16:53:49 -0500

    trying to figure this out

--- src/App.js
+++ src/App.js
@@ -6,19 +6,43 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      
 
     }
   }
   
-
   render() {
+
+    const styles = StyleSheet.create({
+ 
+      container: {
+     
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+      },
+     
+      SquareShapeView: {
+     
+        width: 120,
+        height: 120,
+        backgroundColor: '#00BCD4'
+     
+      },
+     
+      RectangleShapeView: {
+     
+      marginTop: 20,
+      width: 120 * 2,
+      height: 120,
+      backgroundColor: '#FFC107'
+     
+      }
     
     return (
       <div className='container'>
-        <h1>React LiteLite</h1>
         <div className='row'>
-
+          <div className='col-md-6'>$</div>
         </div>
       </div>
     );
--- src/Grid.js
+++ src/Grid.js
@@ -2,23 +2,14 @@ import React, { Component } from 'react';
 
 class Grid extends Component {
     render(){
-        
-        const box = {
-            container: {
-                height: 100px,
-                width: 100px,
-                
-                
-                
-            }
+
+        const border = {
+            'border': this.props.border,
         }
-        
-        
-        
 
         return(
-            <div className='container'>
-                <div className='col-md-6' style={{display: 'inlineBlock', width:'100px', height:'100px'}}></div>
+            <div>
+                
             </div>
         );
        
