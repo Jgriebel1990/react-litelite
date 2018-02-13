0be2343125279b37d86e9cd9f5eeb225e71497e3
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-12 20:15:41 -0500

    created a red box

--- src/App.js
+++ src/App.js
@@ -1,48 +1,29 @@
 import React, { Component } from 'react';
-import Grid from './Grid.js';
+
 
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-
+      box: 
+        {
+          backgroundColor: '',
+          color:'red',
+        }
     }
+    this.handleClick = this.handleClick.bind(this);
   }
-  
+  handleClick(){
+    this.setState =
+  }
   render() {
-
-    const styles = StyleSheet.create({
- 
-      container: {
-     
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-      },
-     
-      SquareShapeView: {
-     
-        width: 120,
-        height: 120,
-        backgroundColor: '#00BCD4'
-     
-      },
-     
-      RectangleShapeView: {
-     
-      marginTop: 20,
-      width: 120 * 2,
-      height: 120,
-      backgroundColor: '#FFC107'
-     
-      }
+   
     
     return (
       <div className='container'>
         <div className='row'>
-          <div className='col-md-6'>$</div>
+          <div onClick={this.setState} className='col-md-1 box' style={this.state.box}></div>
         </div>
       </div>
     );
